fc97d8f2559fc6ab8489862437a25bc711f2e1e0
David <dbutler4@hawk.iit.edu>
2018-09-05 15:31:23 -0500

    Calculates charge, need to add force

--- pithCharge.py
+++ pithCharge.py
@@ -1,10 +1,16 @@
 import math
+import sys
 if __name__ == "__main__":
-    inp = 0;
+    inp = 0
+    m = 4.e-5
+    g = 3.924e-4 
+    k = 9.e-9 
+    theta = 0
     while inp>= 0:
-        inp = input("What is the distance of the pithball from the center?: "
-        theta = math.asin(inp/.0198)
-        ret = math.sqrt((math.degrees(math.sin(theta))*m*g*inp)/k)
+        inp = float(input("What is the distance of the pithball from the center?(cm): "))
+        inp = inp/100
+        theta = math.asin(inp/.198)
+        ret = math.sqrt((math.degrees(math.sin(theta))*m*g*inp*inp)/k)
         print(ret)
 
 
