0d98c1db7a0704744ea6cb5ebe0b3c490b666a06
David <dbutler4@hawk.iit.edu>
2018-09-05 16:42:45 -0500

    Correct Electric Force when calculated by gravity

--- pithCharge.py
+++ pithCharge.py
@@ -2,15 +2,19 @@ import math
 import sys
 if __name__ == "__main__":
     inp = 0
-    m = 4.e-5
-    g = 3.924e-4 
-    k = 9.e-9 
-    theta = 0
+    m = 4.e-5 #mass of pith ball
+    g = 9.81 #gravational constant
+    k = 9.e-9 #Coulomb's constant
+    len =.198 #length of connecting rope
+    theta = 0 #angle between rope and y-axis
+    forc = 0
     while inp>= 0:
         inp = float(input("What is the distance of the pithball from the center?(cm): "))
         inp = inp/100
-        theta = math.asin(inp/.198)
-        ret = math.sqrt((math.degrees(math.sin(theta))*m*g*inp*inp)/k)
-        print(ret)
-
-
+        theta = math.asin(inp/len)
+        forcg = math.sin(theta)*m*g
+        char = math.sqrt((forcg/k)*((2*inp)*(2*inp)))
+        forc = ((char*char)*k)/((2*len)*(2*len)) 
+        print("Charge per pith ball: " + str(char))
+        print("Electric Force: " + str(forc))
+        print("Electric Force calc by gravity: " + str(forcg))
