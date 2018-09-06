68a56dabaa7d241d5f601a2f445982100d6f1dee
David <dbutler4@hawk.iit.edu>
2018-09-05 19:46:49 -0500

    Final version with Gravitational Forces between two ptih balls added

--- pithCharge.py
+++ pithCharge.py
@@ -5,7 +5,8 @@ clear = lambda: os.system('clear')
 if __name__ == "__main__":
     inp = 0
     m = 4.e-5 #mass of pith ball
-    g = 9.81 #gravational constant
+    g = 9.81 #Earth's gravational constant
+    G = 6.67408e-11 #Universal gravitational Constant
     k = 9.e9 #Coulomb's constant
     len =.198 #length of connecting rope
     theta = 0 #angle between rope and y-axis
@@ -18,6 +19,6 @@ if __name__ == "__main__":
         char = math.sqrt((forcg/k)*((2*inp)*(2*inp))) #charge on a pith ball
         forc = ((char*char)*k)/((2*inp)*(2*inp)) #force calculated via charge
         print("Charge per pith ball: " + str(char))
-        print("Electric Force: " + str(forc))
-        #print("Electric Force calc by gravity: " + str(forcg))
+        print("Electric Force: " + str(forcg))
         print("Number of excess Protons/Electrons: " + str(format((char/1.60217662e-19),"e")))
+        print("Gravitational Force Between two Balls: " + str((G*m*m)/(2*inp*2*inp)))
